c78998fe1cd364f7c35a71642dad1807325f8212
wollow12 <62893839+wollow12@users.noreply.github.com>
2024-04-01 22:33:30 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ terraform {
 
   backend "s3" {
     bucket         = "staticwebbucket.com"
-    key            = "state1/terraform.tfstate"
+    key            = "state2/terraform.tfstate"
     region         = "eu-north-1"
     encrypt        = true
-    dynamodb_table = "tf-test"
+    dynamodb_table = "github"
   }
 }
 
@@ -21,7 +21,7 @@ terraform {
 resource "aws_instance" "NewMyServ" {
   ami                    = "ami-0914547665e6a707c"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.web_server_sg_tf.id]
+  vpc_security_group_ids = [aws_security_group.github-test.id]
   key_name               = "master_key"
   tags = {
     Name = "gitlab_testServ"
@@ -29,12 +29,34 @@ resource "aws_instance" "NewMyServ" {
 user_data     =  "${file("script.sh")}"
 }
 
-resource "aws_security_group" "web_server_sg_tf" {
-  name        = "web-server-sg-tf"
-  description = "Allow HTTP/S and SSH to web server"
+resource "aws_security_group" "github-test" {
+  name        = "github-test"
+  description = "Allow SSH to web server"
   vpc_id      = data.aws_vpc.default.id
 }
 
 data "aws_vpc" "default" {
   default = true
 }
+
+resource "aws_security_group_rule" "allow_ssh" {
+  type              = "ingress"
+  description       = "SSH ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.github-test.id
+}
+
+resource "aws_security_group_rule" "allow_all" {
+  type              = "egress"
+  description       = "allow all"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.github-test.id
+}
+
+
